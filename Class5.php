74d33601737bb5ee23a92e727aa0e529f29fa554
zrt <1042252577@qq.com>
2019-06-20 10:36:53 +0800

    fix 面向切面编程

--- Class5.php
+++ Class5.php
@@ -40,12 +40,73 @@ $pipe_arr = [
     'VerfiyAuth',
     'SetCookie'
 ];
+// array_reduce ( array $array , callable $callback [, mixed $initial = NULL ] ) 用法
+// array
+//
+//    输入的 array。
+//callback
+//    callback ( mixed $carry , mixed $item ) : mixed
+//
+//    carry
+//
+//        携带上次迭代里的值； 如果本次迭代是第一次，那么这个值是 initial。
+//    item
+//
+//        携带了本次迭代的值。
+//
+//initial
+//
+//    如果指定了可选参数 initial，该参数将在处理开始前使用，或者当处理结束，数组为空时的最后一个结果
 $callback = array_reduce($pipe_arr,function($stack,$pipe) {
     echo '<pre>';
     print_r($stack);
     echo $pipe;
     echo "<br/>";
     echo '</pre>';
+    // 此处的输出结果
+    // 第一遍
+//    Closure Object
+//    (
+//    )
+
+//    第二遍
+//    上一个闭包函数被单做参数出入 handle（）中
+//    Closure Object
+//    (
+//        [static] => Array
+//    (
+//        [stack] => Closure Object
+//    (
+//    )
+//
+//    [pipe] => VerfiyCsrfToekn
+//        )
+//
+//    )
+
+//    第三遍
+//    同理将第二遍的闭包传入 handle（）中
+//    Closure Object
+//    (
+//        [static] => Array
+//    (
+//        [stack] => Closure Object
+//    (
+//        [static] => Array
+//    (
+//        [stack] => Closure Object
+//    (
+//    )
+//
+//    [pipe] => VerfiyCsrfToekn
+//                        )
+//
+//                )
+//
+//            [pipe] => VerfiyAuth
+//        )
+//
+//    )
     return function() use($stack,$pipe){
         return $pipe::handle($stack);
     };
